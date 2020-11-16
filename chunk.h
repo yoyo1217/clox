7011be6ddbfdb7670ea78a8d512d87752c81d3e8
yohei <yohei@yoheinoMacBook-Pro.local>
2020-11-17 10:53:34 +1100

    add VM

--- chunk.h
+++ chunk.h
@@ -6,6 +6,11 @@
 
 typedef enum{
     OP_CONSTANT,
+    OP_ADD,
+    OP_SUBTRACT,
+    OP_MULTIPLY,
+    OP_DIVIDE,
+    OP_NEGATE,
     OP_RETURN,
 } OpCode;
 
@@ -13,12 +18,14 @@ typedef struct{
     int count;
     int capacity;
     uint8_t* code;
+    int* lines;
     ValueArray constants;
 } Chunk;
 
 void initChunk(Chunk* chunk);
-void writeChunk(Chunk* chunk, uint8_t byte);
-int addConstant(Chunk* chunk, Value value);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
 void freeChunk(Chunk* chunk);
+void writeChunk(Chunk* chunk, uint8_t byte, int line);
+int addConstant(Chunk* chunk, Value value);
 
 #endif
